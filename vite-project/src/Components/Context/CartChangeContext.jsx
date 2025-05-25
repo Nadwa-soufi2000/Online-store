3f8f5b47f0522ce5c2ac73156485dbc7bc1983fb
Nadwa-soufi2000 <145898278+Nadwa-soufi2000@users.noreply.github.com>
2025-05-25 17:26:44 +0300

    Delete vite-project directory

--- vite-project/src/Components/Context/CartChangeContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-export const Cart = createContext(true);
-
-export default function CartChangeContext({children})
-{
-    const[isChange , setIsChange] = useState(true)
-    return <Cart.Provider value={{ isChange , setIsChange}}>{children}</Cart.Provider>
-}
--- vite-project/src/Pages/Website/Writer/Writer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Writer()
-{
-    return(
-        <div>
-            Writer
-        </div>
-    )
-}
